63b362837d92244993a6543ebff636cfb6149bcc
Santiago Martinez <zantumartinez@gmail.com>
2025-08-07 20:12:03 -0300

    details

--- src/Components/ProyectsMain.js
+++ src/Components/ProyectsMain.js
@@ -19,14 +19,16 @@ export default function Projects() {
               alt="Sistema de alquileres" 
             />
                 <div className="go-proy">
-                      <a href="https://sistema-inmobliario.netlify.app/">
+                      <a href="https://sistema-inmobliario.netlify.app/" target="_blank"
+                    rel="noopener noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ww">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
                         </svg>
                           Demo
                           </a>
-                        <a href="https://github.com/santiago-martnez/inmo">
+                        <a href="https://github.com/santiago-martnez/inmo" target="_blank"
+                    rel="noopener noreferrer">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ww">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
                           </svg>
